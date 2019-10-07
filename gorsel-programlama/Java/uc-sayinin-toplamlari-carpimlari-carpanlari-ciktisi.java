3bbdc0f370d8491f85b49c75ee14bcf99dfd2c47
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2019-10-07 11:16:56 +0300

    java-ile-cesitli-basit-ornekler

---
+++ gorsel-programlama/Java/uc-sayinin-toplamlari-carpimlari-carpanlari-ciktisi.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sayi1 = 0, sayi2 = 0, sayi3 = 0;
+        int toplam = 0, carpim = 0, carpan = 0;
+        System.out.println("Lutfen 3 adet sayi giriniz: ");
+
+        Scanner verial = new Scanner(System.in);
+
+        sayi1 = verial.nextInt();
+        sayi2 = verial.nextInt();
+        sayi3 = verial.nextInt();
+
+
+
+        toplam = sayi1 + sayi2 + sayi3;
+        carpim = sayi1 * sayi2 * sayi3;
+
+        System.out.println("Toplamlari "+toplam+ " Carpimlari "+carpim);
+
+        for (int i = 1;i<=sayi1;i++)
+        {
+            carpan = sayi1 / i ;
+            if (sayi1 % i == 0 ){
+                System.out.println(sayi1+"'in carpanlari: "+carpan);
+            }
+
+        }
+        for (int i = 1;i<=sayi2;i++)
+        {
+            carpan = sayi2 / i ;
+            if (sayi2 % i == 0 ){
+                System.out.println(sayi2+"'in carpanlari: "+carpan);
+            }
+
+        }
+        for (int i = 1;i<=sayi3;i++)
+        {
+            carpan = sayi3 / i ;
+            if (sayi3 % i == 0 ){
+                System.out.println(sayi3+"'in carpanlari: "+carpan);
+            }
+
+        }
+
+
+
+    }
+}
