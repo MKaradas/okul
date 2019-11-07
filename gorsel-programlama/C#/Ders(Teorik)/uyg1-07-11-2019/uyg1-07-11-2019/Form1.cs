1634d9229988bd0802ca0f107e16ee9c04cea5df
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2019-11-07 10:43:04 +0300

    dersde-commit-keyfi:)

---
+++ gorsel-programlama/C#/Ders(Teorik)/uyg1-07-11-2019/uyg1-07-11-2019/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uyg1_07_11_2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Fibonacci Hesapla";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int deger = Convert.ToInt32(textBox1.Text);
+            int[] dizi = new int[deger];
+            dizi[0] = 0;
+            dizi[1] = 1;
+            int i = 2;
+            if (deger<2)
+            {
+                MessageBox.Show("Yanlış Değer Girdiniz!");
+            }
+            else { 
+            for (i=2;i<dizi.Length;i++)
+            {
+                dizi[i] = dizi[i-1]+dizi[i-2];
+            }
+            MessageBox.Show("Serideki boyut -> "+deger+" sonuc -> "+dizi[deger-1]);
+            }
+        }
+    }
+}
---
+++ gorsel-programlama/C#/Ders(Teorik)/uyg2-07-11-2019/uyg2-07-11-2019/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uyg2_07_11_2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] dizia = new int[2,3] { {1,2,1}, {0,3,-1} };
+            int[,] dizib = new int[3,2] { {1,2}, {1,5}, {2,3} };
+            int[,] sonucdizi = new int[dizia.GetLength(0),dizib.GetLength(1)];
+            //listBox1.Text = "";
+            string satir = "";
+
+            for (int i = 0; i < dizia.GetLength(0); i++)
+            {
+                for (int j = 0; j < dizib.GetLength(1); j++)
+                {
+                    for (int k=0;k<dizia.GetLength(1);k++)
+                    {
+                        sonucdizi[i, j] += dizia[i, k] * dizib[k, j];
+                    }
+                    satir += sonucdizi[i, j].ToString()+" ";
+                    if (j == dizib.GetLength(1) - 1)
+                    {
+                        listBox1.Items.Add(satir);
+                        satir = "";
+                    }
+                }
+            }
+        }
+            
+
+        }
+    }
+
