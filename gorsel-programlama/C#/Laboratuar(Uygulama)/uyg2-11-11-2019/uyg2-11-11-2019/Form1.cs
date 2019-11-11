feba8daa8b323e26baa9435fe8f6d1a5be290b3d
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2019-11-11 13:00:53 +0300

    ders-ici-yapilan-uygulama-eklendi

---
+++ gorsel-programlama/C#/Laboratuar(Uygulama)/uyg2-11-11-2019/uyg2-11-11-2019/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uyg2_11_11_2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int saat = 0, dakika = 0, saniye = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            label1.Text = saniye.ToString();
+            this.BackColor = Color.Gold;
+            if (saniye==59)
+            {
+                dakika++;
+                label2.Text = dakika.ToString();
+                this.BackColor = Color.PeachPuff;
+                saniye = 0;
+                progressBar1.Value += 10;
+                if (progressBar1.Value==100)//önlem alman lazım
+                {
+                    this.Close();
+                }
+                if (dakika==59)
+                {
+                    saat++;
+                    label3.Text = saat.ToString();
+                    this.BackColor = Color.OrangeRed;
+                    dakika = 0;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
---
+++ gorsel-programlama/C#/Laboratuar(Uygulama)/uyg3-11-11-2019/uyg3-11-11-2019/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uyg3_11_11_2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int saniye = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saniye = Convert.ToInt32(textBox1.Text);
+            timer1.Enabled = true;
+            //veya bu timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye--;
+            
+            progressBar1.Value += 10;
+            if (saniye<4)
+            {
+                label2.Text = saniye.ToString();
+            }
+            if (saniye==0)
+            {
+                this.Close();
+            }
+            else
+            {
+                this.Text="uygulama "+saniye.ToString() + " sonra kapanacaktır.";
+            }
+        }
+    }
+}
