c80cec4fab05dea84298304c267febdedef0963d
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2019-11-14 07:40:01 +0300

    ders-ici-yapilan-uygulama-eklendi

---
+++ gorsel-programlama/C#/Ders(Teorik)/uyg1-14-11-2019/uyg1-14-11-2019/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uyg1_14_11_2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = richTextBox1.Text.Replace('A','é');
+            richTextBox1.Text = richTextBox1.Text.Replace('B', 'i');
+            richTextBox1.Text = richTextBox1.Text.Replace('C', 'ğ');
+            richTextBox1.Text = richTextBox1.Text.Replace('D', 'Z');
+            richTextBox1.Text = richTextBox1.Text.Replace('E', 'æ');
+            richTextBox1.Text = richTextBox1.Text.Replace('F', 'Ö');
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = richTextBox1.Text.Replace('é', 'A');
+            richTextBox1.Text = richTextBox1.Text.Replace('i', 'B');
+            richTextBox1.Text = richTextBox1.Text.Replace('ğ', 'C');
+            richTextBox1.Text = richTextBox1.Text.Replace('Z', 'D');
+            richTextBox1.Text = richTextBox1.Text.Replace('æ','E');
+            richTextBox1.Text = richTextBox1.Text.Replace('Ö', 'F');
+        }
+    }
+}
---
+++ gorsel-programlama/C#/Ders(Teorik)/uyg2-14-11-2019/uyg2-14-11-2019/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uyg2_14_11_2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            int toplam = 0;
+            for (int i = 0; i <str.Length; i++)
+            {
+                if (Char.IsNumber(str,i)==true)
+                {
+                    textBox2.Text += str[i]+"+";
+                    toplam += Convert.ToInt32(str[i].ToString());
+                }
+            }
+
+            if (toplam == 0)
+            {
+                textBox2.Text = "Girilen Metinde Hic Numerik Deger Yoktur.";
+            }
+            else
+            {
+               // textBox2.Text = textBox2.Text.Remove(textBox2.Text.Length);//burası güncellenecektir.
+                textBox2.Text += " = " + toplam.ToString();
+            }
+
+        }
+    }
+}
