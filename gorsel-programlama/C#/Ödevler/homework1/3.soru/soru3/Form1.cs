6b8bd7d3060750b511016aa2bb5018eb8bf72bdd
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2019-11-27 22:52:31 +0300

    name-fix

---
+++ gorsel-programlama/C#/Ödevler/homework1/3.soru/soru3/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace soru3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "İdeal Kilo Hesabı";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ad = Convert.ToString(textBox1.Text);
+            string soyad = Convert.ToString(textBox2.Text);
+            double yas = Convert.ToDouble(textBox3.Text);
+            double boy = Convert.ToDouble(textBox4.Text);
+            double kilo = Convert.ToDouble(textBox6.Text);
+            double idealkilo=0;
+            string cinsiyet = Convert.ToString(comboBox1.Text);
+
+            if (cinsiyet == "Erkek")
+            {
+                idealkilo = (boy-100 + yas/10) * 0.9;
+                textBox8.Text = idealkilo.ToString();
+            }
+            else if (cinsiyet == "Kadın")
+            {
+                idealkilo = (boy - 100 + yas / 10) * 0.8;
+                textBox8.Text = idealkilo.ToString();
+            }
+
+            if (kilo == idealkilo)
+            {
+                label9.Text = "Bravo İdeal kilodasınız";
+            }
+            else if (kilo < idealkilo)
+            {
+                label9.Text = "Zayıfsın "+(idealkilo-kilo)+" kilo almalısın!";
+            }
+            else if (kilo > idealkilo)
+            {
+                label9.Text = "Şişmansın " + (kilo - idealkilo) + " kilo zayıflamalısın!";
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox6.Text = "";
+            textBox8.Text = "";
+            comboBox1.Text = "";
+            
+        }
+    }
+}
---
+++ gorsel-programlama/C#/Ödevler/homework1/5.soru/soru5/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace soru5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Hesap Makinesi";
+        }
+
+        double sayi1=0, sayi2=0, sonuc=0;
+        char islemBilgisi = ' ';
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text += "5";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text += "9";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text += "8";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text += "7";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text += "6";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "0";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text += "4";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text += "3";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text += "2";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text += "1";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            sayi1 = 0;
+            sayi2 = 0;
+            sonuc = 0;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            islemBilgisi = '+';
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            islemBilgisi = '*';
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";  
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            islemBilgisi = '/';
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            islemBilgisi = '-';
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text!="")
+            {
+                sayi2 = Convert.ToDouble(textBox1.Text);
+                switch (islemBilgisi)
+                {
+                    case '+':
+                        sonuc = sayi1 + sayi2;
+                        textBox1.Text = sonuc.ToString();
+                        break;
+                    case '-':
+                        sonuc = sayi1 - sayi2;
+                        textBox1.Text = sonuc.ToString();
+                        break;
+                    case '*':
+                        sonuc = sayi1 * sayi2;
+                        textBox1.Text = sonuc.ToString();
+                        break;
+                    case '/':
+                        sonuc = sayi1 / sayi2;
+                        textBox1.Text = sonuc.ToString();
+                        break;
+                    default:
+                        MessageBox.Show("Lutfen bir islem seciniz!!");
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lutfen Sayi Giriniz!");
+            }
+          
+        }
+    }
+}
